70d7847d193da4e3dc67030a606113b782acb5ca
Richard Benjamin <richardbenjamin@me.com>
2025-10-01 15:02:21 -0700

    Added a11y to linting

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,14 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   { ignores: ['dist', 'coverage'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,9 +20,11 @@ export default tseslint.config(
       'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       'arrow-parens': ['error', 'as-needed', { 'requireForBlockBody': true }],
       'comma-dangle': ['error', 'always-multiline'],
       'curly': ['error', 'all'],
@@ -31,4 +35,4 @@ export default tseslint.config(
       'react/jsx-one-expression-per-line': ['error', { allow: "non-jsx" }],
     },
   },
-)
+]
