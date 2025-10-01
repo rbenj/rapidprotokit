2feba21836207587350c16384d613a0a21fa6bfc
Richard Benjamin <richardbenjamin@me.com>
2025-10-01 14:53:07 -0700

    Added vitest, cypress, and some customization

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist', 'coverage'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react': react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'arrow-parens': ['error', 'as-needed', { 'requireForBlockBody': true }],
+      'comma-dangle': ['error', 'always-multiline'],
+      'curly': ['error', 'all'],
+      'no-console': 'error',
+      'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': false }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/jsx-max-props-per-line': ['error', { 'maximum': 3 }],
+      'react/jsx-one-expression-per-line': ['error', { allow: "non-jsx" }],
+    },
+  },
+)
